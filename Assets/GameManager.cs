03ed9b89eaf6e27429183b141f1ea9dab180c4d4
MrNerix <84454507+MrNerix@users.noreply.github.com>
2024-10-01 15:21:01 +0200

    Game Manager Collects all coordinates
    coordinates

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,11 +1,21 @@
+using Unity.XR.CoreUtils;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject redChecker;
+    public GameObject blackChecker;
+    public GameObject coordianteGameObject;
+    public GameObject[] coordinates;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        int coordCount = coordianteGameObject.transform.childCount;
+        coordinates = new GameObject[coordCount];
+        for (int i = 0; i< coordCount; i++){
+            coordinates[i] = coordianteGameObject.transform.GetChild(i).gameObject;
+        }
     }
 
     // Update is called once per frame
