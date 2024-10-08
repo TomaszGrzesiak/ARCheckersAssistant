a5a7945a6b3d1d39c8c216dbfe71c12fe597fa1a
MrNerix <84454507+MrNerix@users.noreply.github.com>
2024-10-08 11:04:47 +0200

    Added checkers/game functionality
    Added PreLoad fucntion to load all the checkers and added PlaceChecker to place checkers

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using Unity.XR.CoreUtils;
 using UnityEngine;
 
@@ -11,16 +12,40 @@ public class GameManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        int coordCount = coordianteGameObject.transform.childCount;
-        coordinates = new GameObject[coordCount];
-        for (int i = 0; i< coordCount; i++){
-            coordinates[i] = coordianteGameObject.transform.GetChild(i).gameObject;
-        }
+        PreLoad();
+        PlaceChecker("Black", "2B");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    void PlaceChecker(string type, string position){
+        for (int i = 0; i < coordinates.Length; i++)
+        {
+            if(position.Equals(coordinates[i].name)){
+                if (type.Equals("Red"))
+                coordinates[i].gameObject.transform.GetChild(0).gameObject.SetActive(true);
+                else if (type.Equals("Black"))
+                coordinates[i].gameObject.transform.GetChild(1).gameObject.SetActive(true);
+            }
+        }
+    }
+
+    void PreLoad(){
+        int coordCount = coordianteGameObject.transform.childCount;
+        coordinates = new GameObject[coordCount];
+        for (int i = 0; i < coordCount; i++)
+        {
+            coordinates[i] = coordianteGameObject.transform.GetChild(i).gameObject;
+        }
+        for (int i = 0; i < coordinates.Length; i++)
+        {
+            Instantiate(redChecker, coordinates[i].transform.gameObject.transform);
+            coordinates[i].gameObject.transform.GetChild(0).gameObject.SetActive(false);
+            Instantiate(blackChecker, coordinates[i].transform.gameObject.transform);
+            coordinates[i].gameObject.transform.GetChild(1).gameObject.SetActive(false);
+        }
     }
 }
